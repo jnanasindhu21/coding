5f0ad5ea09b18677b799a8fe0e01b680ddaf2c32
Jnana Sindhu K S <96429100+jnanasindhu21@users.noreply.github.com>
2026-07-30 21:49:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of an Array/size-of-an-array.js
@@ -0,0 +1,11 @@
+class Solution {
+    getSize(arr) {
+        // code here
+        return arr.length;
+        // let count = 0;
+        // while(arr[count]) {
+        //     count++;
+        // }
+        // return count;
+    }
+}
