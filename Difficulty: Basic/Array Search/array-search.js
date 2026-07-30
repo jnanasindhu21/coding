9aacd30ca2bb998afa00ce7b890d862b0adbec06
Jnana Sindhu K S <96429100+jnanasindhu21@users.noreply.github.com>
2026-07-30 21:46:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.js
+++ Difficulty: Basic/Array Search/array-search.js
@@ -1,13 +1,8 @@
-/**
- * @param {number[]} arr
- * @param {number} x
- * @return {number}
- */
 class Solution {
     search(arr, x) {
         // code here
-        for ( let i = 0; i < arr.length; i++) {
-            if( x === arr[i] ) {
+        for( let i =0 ; i<arr.length; i++ ) {
+            if(x === arr[i]) {
                 return i;
             }
         }
