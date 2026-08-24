8a3af81485f8039eca638af59265c5d5b027b2b6
Jnana Sindhu K S <96429100+jnanasindhu21@users.noreply.github.com>
2026-08-24 21:46:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.js
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.js
@@ -6,11 +6,6 @@
 class Solution {
     reverseArray(arr) {
         // code here
-        let n = arr.length;
-        for(let i = 0; i < n/2; i++) {
-            let t = arr[i];
-            arr[i] = arr[n-i-1];
-            arr[n-i-1] = t;
-        }
+        return arr.reverse();
     }
 }
