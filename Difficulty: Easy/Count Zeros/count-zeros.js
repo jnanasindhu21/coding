8f39708322ebcad5594b1d90fbddd88c76bafdd8
Jnana Sindhu K S <96429100+jnanasindhu21@users.noreply.github.com>
2026-08-24 22:00:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Zeros/count-zeros.js
@@ -0,0 +1,13 @@
+class Solution {
+    countZeroes(arr) {
+        // code here
+        let count = 0;
+        
+        for (let i=0; i<arr.length; i++) {
+            if(arr[i] === 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
